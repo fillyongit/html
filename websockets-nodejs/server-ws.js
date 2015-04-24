e876f2b4bfa65828fbcff6391296d64e58c2b709
alessio <filippini.alessio@gmail.com>
2015-04-24 17:20:46 +0200

    Work in progress...

--- websockets-nodejs/server-ws.js
+++ websockets-nodejs/server-ws.js
@@ -1,44 +1,79 @@
 /**
  * Eseguire come utente apache se si vuoe poi che uno script php possa scrivere sullo stesso local socket del server.
  * sudo -u www-data /path/to/node server-ws.js
+* /https://github.com/websockets/ws
  */
-var net = require('net');
-    WebSocketServer = require('ws').Server
-// https://github.com/websockets/ws
+var net = require('net'),
+    WebSocketServer = require('ws').Server,
+    log4js = require('log4js'),
+    fs = require('fs');
+
+console.log(__dirname + '/server-ws.log');
+
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file(__dirname + '/server-ws.log'), 'serverws');
+var logger = log4js.getLogger('serverws');
+logger.setLevel('INFO');
+
+var socketPath = '/tmp/bac.sock';
+
+process.on('exit', function(code) {
+	fs.unlinkSync(socketPath);
+	logger.info('Node esce con il codice: ' + code);
+});
+process.on('SIGINT', function() {
+	// Rimuove il socket.
+	logger.info('Processo interrotto.');
+	process.exit(0);
+});
+process.on('SIGBREAK', function() {
+	// Su windows.
+	logger.info('Processo interrotto.');
+	process.exit(0);
+});
+
+//var adminWS = [];
  
-var adminWS = [];
- 
-var notify = function(event) {
+/*var notify = function(event) {
   for(c in adminWS)
     c.send(JSON.stringify({
       time: (new Date()).getTime()
     }));
-};
+};*/
  
+// Rimuove il file socket.
+fs.unlink(socketPath, function(err){
+	if(err)
+		logger.error(err);
+});
+
 var server = net.createServer(function(socket){
 	// Connection callback. E' come server.on('connection', function(socket){ ... });
 	// Socket è un ReadableStream.
 	socket.on('data', function(buffer){
-		console.log('data received...' + buffer.toString() + ' ' + buffer.length);
+		logger.info('data received...' + buffer.toString() + ' ' + buffer.length);
 		//notify(buffer);
 		wss.broadcast(buffer.toString());
 	});
 	socket.on('end', function(){
-		console.log('client disconnected');
+		logger.info('client disconnected');
 	});
 });
-server.listen('/tmp/bac.sock', function(){
-	console.log('Server bound on %j', server.address());
+server.listen(socketPath, function(){
+	logger.info('Server bound on %j', server.address());
+});
+server.on('close', function (e) {
+	logger.error('Server closed.');
 });
 server.on('error', function (e) {
 	  if (e.code == 'EADDRINUSE') {
-	    console.log('Server socket in use!');
+	    logger.error('Server socket in use!');
 	  }
 });
 
 var wss = new WebSocketServer({port: 8080}, function(err, data) {
 	// Listening callback.
-	console.log('WebSocketServer listening...');
+	logger.info('WebSocketServer listening...');
 });
 wss.broadcast = function broadcast(data) {
 	  wss.clients.forEach(function each(client) {
@@ -46,14 +81,14 @@ wss.broadcast = function broadcast(data) {
 	  });
 };
 wss.on('connection', function(ws) {
-	console.log('Nuova connessione');
+	logger.info('Nuova connessione');
 //	setInterval(function() {
 //		ws.send(JSON.stringify({msg: 'connessione avvenuta'})); 
 //	}, 2000);
-    adminWS.push(ws);
+    //adminWS.push(ws);
 });
 wss.on('error', function (e) {
 	  if (e.code == 'EADDRINUSE') {
-	    console.log('Address in use!');
+	    logger.error('Address in use!');
 	  }
 });
